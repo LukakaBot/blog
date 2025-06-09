a52da508eea917d6c2d8156bfc19e84d8822d350
鲨鱼辣椒 <monishine_code@163.com>
2025-06-09 15:28:56 +0800

    feat: astro config

--- astro.config.js
+++ astro.config.js
@@ -17,14 +17,15 @@ import { site } from './src/config.json'
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 import swup from '@swup/astro'
-import { loadEnv } from 'vite'
+// import { loadEnv } from 'vite'
 
-const { PUBLIC_APP_BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+// const { PUBLIC_APP_BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
   site: site.url,
-  base: PUBLIC_APP_BASE_URL,
+  // base: PUBLIC_APP_BASE_URL,
+  base: '/blog',
   integrations: [
     tailwind(),
     react(),
